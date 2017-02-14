48cb866912ae00597c7e1985121d2d227e188cd1
David Schultz <davids24@gmail.com>
2017-02-14 15:47:30 -0600

    monitor if a website goes down, and notify slack

---
+++ updown_monitor/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import os
+import logging
+from functools import partial
+from argparse import ArgumentParser
+
+def main(testing=False):
+    from slack import SlackMessage
+    from updown import monitor
+
+    logging.basicConfig(level='DEBUG' if testing else 'INFO')
+    
+    with open('.slack_token') as f:
+        token = f.read().strip()
+    slack = SlackMessage(token, testing=testing)
+    #slack.run()
+    monitor([
+        {'server':'https://iceprod2.icecube.wisc.edu',
+         'send':partial(slack.send_message,'iceprod2')},
+        {'server':'https://sub-simprod-2.icecube.wisc.edu:9080',
+         'send':partial(slack.send_message,'iceprod2')},
+    ])
+    logging.error('updown monitor has stopped')
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--testing', action='store_true',
+                        help='testing mode')
+
+    args = parser.parse_args()
+    main(testing=args.testing)
---
+++ updown_monitor/updown.py
@@ -0,0 +1,33 @@
+import logging
+import time
+
+import requests
+
+logger = logging.getLogger('updown')
+
+def monitor(servers, delay=60, failure_thresh=5):
+    """
+    Monitor a set of servers.
+    
+    Args:
+        servers (list): list of dicts to monitor/send
+        delay (int): sleep delay between checks
+        failure_thresh (int): number of page failures before error
+    """
+    for s in servers:
+        s['failures'] = 0
+    while True:
+        start = time.time()
+        for s in servers:
+            try:
+                r = requests.get(s['server'], verify=False, timeout=10)
+                r.raise_for_status()
+            except:
+                logger.warn('error getting server page', exc_info=True)
+                s['failures'] += 1
+                if s['failures'] == failure_thresh:
+                    s['send'](s+' is down')
+            else:
+                s['failures'] = 0
+
+        time.sleep(delay-(time.time()-start))
